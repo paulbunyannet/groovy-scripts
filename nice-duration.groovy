8578fb30f7a83a0eaf3d89dd4d2ceb3497d3a4f4
Paul Bunyan Communications <naten@paulbunyan.net>
2016-12-08 10:51:34 -0600

    fixed spelling error

--- nice-duration.groovy
+++ nice-duration.groovy
@@ -4,7 +4,7 @@
 
 def version = '1.0'
 
-def niceDuraton(duration) {
+def niceDuration(duration) {
   def seconds = "echo \$(((\$(date +%s%N | cut -b1-13)-${duration})/1000));".text
    // http://unix.stackexchange.com/a/217604
   def hms = "echo \"\$(date -d \"1970-01-01 + ${seconds} seconds\" \"+%H hours %M minutes %S seconds\")\"".text
