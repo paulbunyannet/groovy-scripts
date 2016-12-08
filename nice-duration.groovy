ee295647aa61bff4b96872c71fcb42abdb7fdc0f
Paul Bunyan Communications <naten@paulbunyan.net>
2016-12-08 11:10:44 -0600

    updated

--- nice-duration.groovy
+++ nice-duration.groovy
@@ -5,10 +5,12 @@
 def version = '1.0'
 
 def niceDuration(duration) {
-  def seconds = "echo \$(((\$(date +%s%N | cut -b1-13)-${duration})/1000));".text
+  def seconds = "echo \$(((\$(date +%s%N | cut -b1-13)-${duration})/1000));".execute();
+  def buffer = new StringBuffer()
+  proc.consumeProcessErrorStream(buffer)
    // http://unix.stackexchange.com/a/217604
-  def hms = "echo \"\$(date -d \"1970-01-01 + ${seconds} seconds\" \"+%H hours %M minutes %S seconds\")\"".text
-  return hms
+  def hms = "echo \"\$(date -d \"1970-01-01 + ${seconds.text} seconds\" \"+%H hours %M minutes %S seconds\")\"".execute();
+  return hms;
 }
 
 return this;
