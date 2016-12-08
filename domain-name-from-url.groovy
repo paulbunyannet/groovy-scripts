78a6cdc88b39a9cfb80e3b88b4df41c7d932d8a5
Paul Bunyan Communications <naten@paulbunyan.net>
2016-12-08 11:47:09 -0600

    created new groovy file

---
+++ domain-name-from-url.groovy
@@ -0,0 +1,18 @@
+
+/**
+ * return domain name from url
+ * Example:
+ * http://
+ */
+
+def version = '1.0'
+
+def domainNameFromUrl(url) {
+  def base = ["bash", "-c", "echo ${url} | cut -d'/' -f3 | cut -d':' -f1"].execute();
+  def buffer = new StringBuffer();
+  base.consumeProcessErrorStream(buffer);
+  def url = base.text;
+  return url.trim();
+}
+
+return this;
