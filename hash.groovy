76af35d32d40e0d7bf95d531b7662c51b057aedb
Nate Nolting <me@natenolting.com>
2017-06-23 18:33:19 -0500

    update

--- hash.groovy
+++ hash.groovy
@@ -3,7 +3,7 @@ def version = '1.0'
 import java.security.MessageDigest
 
 //https://gist.github.com/ikarius/299062#gistcomment-1463377
-def generateMD5_A(String s){
+def generateMD5(String s){
     MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
 }
 
