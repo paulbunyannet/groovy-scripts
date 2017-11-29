6a8ff7cc0d187240a6d3d153a51fde4b6d5a126b
Nate Nolting <me@natenolting.com>
2017-11-29 09:45:49 -0600

    Tweaked readable file size script.

--- readable-file-size.groovy
+++ readable-file-size.groovy
@@ -4,12 +4,14 @@
  */
 
 def version = '1.0'
+import java.text.DecimalFormat;
 
 public static String readableFileSize(long size) {
     if(size <= 0) return "0";
-    final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+    final String[] units = ["B","kB","MB","GB","TB"] as  String[];
     int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
     return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
 }
 
 return this;
+​
