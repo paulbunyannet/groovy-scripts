f1ff1bd584f54bec051f236e78ec94b02e5d7e9b
Nate Nolting <me@natenolting.com>
2018-01-29 11:29:24 -0600

    Added gitlab groovy file

---
+++ gitlab.groovy
@@ -0,0 +1,28 @@
+
+def version = '1.0'
+
+/**
+ * Is the current gitlab user master and using the develop branch?
+ * @param string master the master user
+ */
+def isMainRepoAndDevelop(String master="Web Development"){
+    echo "gitlabSourceNamespace ${gitlabSourceNamespace} | gitlabTargetBranch ${gitlabTargetBranch}"
+    if ("${gitlabSourceNamespace}" == master && "${gitlabTargetBranch}" == "develop") {
+      return true
+    }
+    return false
+}
+
+/**
+ * Is the current gitlab user master and using the master branch?
+ * @param string master the master user
+ */
+def isMainRepoAndMaster(String master="Web Development"){
+    echo "gitlabSourceNamespace ${gitlabSourceNamespace} | gitlabTargetBranch ${gitlabTargetBranch}"
+    if ("${gitlabSourceNamespace}" == master && "${gitlabTargetBranch}" == "master") {
+      return true
+    }
+    return false
+}
+
+return this;
