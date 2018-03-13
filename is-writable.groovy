3bf8bdcbd37968e956fe2fe7a84d2c2feb152b95
Nate Nolting <me@natenolting.com>
2018-03-13 16:36:54 -0500

    Run checks from inside the job workspace.

--- is-writable.groovy
+++ is-writable.groovy
@@ -8,24 +8,24 @@ import java.io.File
 def isWritable(String isWritableFolder) {
     echo "Running the isWritable method with folder ${isWritableFolder}"
     // make folders if it does not exist
-    File wf = new File(isWritableFolder)
+    File wf = new File("${env.WORKSPACE}/${isWritableFolder}")
     if (!wf.exists()) {
         wf.mkdirs()
     }
 
     // Set the ownership of that folder to the www-data user
-    sh "docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
+    sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
 
     // Set the permissions for the www-data user user
-    sh "docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
+    sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
 
     if (File.notExists("isWritable.php")) {
         // get the writable script
         echo "Getting the permissions checking script"
-        sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > isWritable.php"
+        sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > ${env.WORKSPACE}/isWritable.php"
     }
     // do a check that the www-data user can write to this folder now.
-    sh "docker-compose exec -T code bash -c \"php isWritable.php --dir=${isWritableFolder}\""
+    sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"php isWritable.php --dir=${isWritableFolder}\""
 }
 
 return this
