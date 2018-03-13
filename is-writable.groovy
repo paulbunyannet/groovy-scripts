6e8fdb1d09c2663041261f081824f9bd3e78a708
Nate Nolting <me@natenolting.com>
2018-03-13 16:00:03 -0500

    Tweaking the checks for folders if not already set.

--- is-writable.groovy
+++ is-writable.groovy
@@ -6,22 +6,25 @@ def version = '1.0'
  * @param string isWritableFolder the folder to check
  */
 def isWritable(isWritableFolder) {
-  // If the folder doesn't already exist, then create it
-  if(File.notExists(isWritableFolder)) {
-    writableFolder = new File(isWritableFolder)
-    writableFolder.mkdirs()
-  }
 
-  if(File.notExists("isWritable.php")) {
-    // get the writable script
-    sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > isWritable.php"
-  }
-  // Set the ownership of that folder to the www-data user
-  sh "docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
-  // Set the permissions for the www-data user user
-  sh "docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
-  // do a check that the www-data user can write to this folder now.
-  sh "docker-compose exec -T code bash -c \"php isWritable.php --dir=${isWritableFolder}\""
+    // make folders if it does not exist
+    if (File.notExists(isWritableFolder)) {
+        sh "docker-compose exec -T code bash -c \"mkdir -p ${isWritableFolder}\""
+    }
+
+    // Set the ownership of that folder to the www-data user
+    sh "docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
+
+    // Set the permissions for the www-data user user
+    sh "docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
+
+    if (File.notExists("isWritable.php")) {
+        // get the writable script
+        echo "Getting the permissions checking script"
+        sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > isWritable.php"
+    }
+    // do a check that the www-data user can write to this folder now.
+    sh "docker-compose exec -T code bash -c \"php isWritable.php --dir=${isWritableFolder}\""
 }
 
-return this;
+return this
