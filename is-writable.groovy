7139a5582f26dadd95999db3ce5ae8a67dc42bdf
Nate Nolting <me@natenolting.com>
2018-03-13 16:39:41 -0500

    Run checks from inside the job workspace.

--- is-writable.groovy
+++ is-writable.groovy
@@ -3,26 +3,29 @@ import java.io.File
 /**
  * Check if folder is writable. Optionally make the folder if
  * it does not already exist.
- * @param string isWritableFolder the folder to check
+ * @param String isWritableFolder the folder to check
+ * @param String basePath the folder to work in (workspace)
  */
-def isWritable(String isWritableFolder) {
+def isWritable(String isWritableFolder, String basePath) {
+
     echo "Running the isWritable method with folder ${isWritableFolder}"
+
     // make folders if it does not exist
-    File wf = new File("${env.WORKSPACE}/${isWritableFolder}")
+    File wf = new File("${basePath}/${isWritableFolder}")
     if (!wf.exists()) {
         wf.mkdirs()
     }
 
     // Set the ownership of that folder to the www-data user
-    sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
+    sh "cd ${basePath}; docker-compose exec -T code bash -c \"chown -R www-data:www-data ${isWritableFolder}\""
 
     // Set the permissions for the www-data user user
-    sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
+    sh "cd ${basePath}; docker-compose exec -T code bash -c \"chmod 755 -fR ${isWritableFolder}\""
 
-    if (File.notExists("isWritable.php")) {
+    if (File.notExists("${basePath}/isWritable.php")) {
         // get the writable script
         echo "Getting the permissions checking script"
-        sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > ${env.WORKSPACE}/isWritable.php"
+        sh "curl --silent -k https://gitlab.paulbunyan.net/snippets/9/raw > ${basePath}/isWritable.php"
     }
     // do a check that the www-data user can write to this folder now.
     sh "cd ${env.WORKSPACE}; docker-compose exec -T code bash -c \"php isWritable.php --dir=${isWritableFolder}\""
