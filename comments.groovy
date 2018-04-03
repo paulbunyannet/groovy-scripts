0ec21b55b0dcdf270766cc8ecbf90205511cd28f
Nate Nolting <me@natenolting.com>
2018-04-03 14:41:22 -0500

    Trying a different split method

--- comments.groovy
+++ comments.groovy
@@ -1,14 +1,23 @@
 #!/usr/bin/groovy
 
 def commentBlock(String title="", String body="" ) {
-	Integer length = 75
-	String parsedBody = body.replaceAll("(.{${length})", "$1\n")
+	Integer lineLength = 75
+	StringBuilder sb = new StringBuilder()
+	for (int i = 0; i < body.length(); i++) {
+		if (i > 0 && (i % lineLength == 0)) {
+			sb.append("\n")
+		}
+
+		sb.append(body.charAt(i))
+	}
+
+	body = sb.toString()
 	echo "    /*\n" +
 			"    |--------------------------------------------------------------------------\n" +
 			"    | ${title}\n" +
 			"    |--------------------------------------------------------------------------\n" +
 			"    |\n" +
-			"    | ${parsedBody}\n" +
+			"    | ${body}\n" +
 			"    */"
 }
 return this
