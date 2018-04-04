99b0ad54898fce15dc1c71ac6415e0e018ec0258
Nate Nolting <me@natenolting.com>
2018-04-04 00:38:45 -0500

    Reverted testing.groovy

--- testing.groovy
+++ testing.groovy
@@ -2,57 +2,57 @@
 
 /**
  * Prep and run tests with codeception
- * @param baseString The folder to run tests in
- * @param container The container to run the tests in
- * @param permissions Permissions to use when making items writable
- * @param upstreamUser The name of the upstream user, used to check whether we should be running tests verbosely or not
+ * @param params
+ * Params keys:
+ * baseString 	-> The folder to run tests in
+ * container 	-> The container to run the tests in
+ * permissions	-> Permissions to use when making items writable
+ * upstreamUser -> The name of the upstream user, used to check whether we should be running tests verbosely or not
+ * comments 	-> instance of comments.groovy
+ * isWritable 	-> instance of is-writable.groovy
+ * gitlab 		-> instance of gitlab.groovy
  * @return
  */
 //String basePath = env.WORKSPACE, String container = "code", String permissions = "www-data:www-data", String upstreamUser = "Web Development"
-def runCodeceptionTests(String basePath, String container, String permissions, String upstreamUser ) {
+def runCodeceptionTests(Object params) {
 	try {
-
-		File tempDir = new File("${basePath}/tmp")
-		if (!tempDir.exists()) {
-			tempDir.mkdirs()
-		}
-		String groovies = "gitlab.groovy,is-writable.groovy,comments.groovy"
-		def getGroovies = groovies.split(',')
-		getGroovies.each {
-			sh "curl --silent -k https://raw.githubusercontent.com/paulbunyannet/groovy-scripts/master/${it} > ${tempDir.toString()}/${it}"
-			evaluate(new File("${tempDir.toString()}/${it}"))
-		}
-
+		String container 	= params.container
+		String basePath 	= params.basePath
+		String permissions 	= params.permissions
+		String upstreamUser = params.upstreamUser
+		def comments 		= params.comments
+		def isWritable 		= params.isWritable
+		def gitlab 			= params.gitlab
 	} catch (intError) {
 		return error(intError.getMessage())
 	}
 
-	commentBlock("Starting Tests", "Running tests in the ${container} container!" as String)
+	comments.commentBlock("Starting Tests", "Running tests in the ${container} container!" as String)
 
 	try {
 		//migrate before running anything
 		// if artisan exists then run migrations
 		File artisan = new File("${basePath}/artisan")
 		if (artisan.exists() && artisan.isFile()) {
-			commentBlock("Migrations", "Running migrations with Artisan")
+			comments.commentBlock("Migrations", "Runing migrations with Artisan")
 			sh "cd ${basePath}; docker-compose exec -T ${container} bash -c \"php artisan migrate\""
 		}
 
 		// If a generic migration script exists then try and run it
 		File genericMigrate = new File("${basePath}/migrate.php")
 		if (!artisan.exists() && genericMigrate.exists() && genericMigrate.isFile()) {
-			commentBlock("Migrations", "Running the generic migration runner ${genericMigrate.toString()}")
+			comments.commentBlock("Migrations", "Running the generic migration runner ${genericMigrate.toString()}")
 			sh "cd ${basePath}; docker-compose exec -T ${container} bash -c \"php migrate.php\""
 		}
 		String writable = "cache,storage,tests"
 		def makeWritableFolders = writable.split(',')
-		commentBlock("Writable", "Making the folders ${basePath}/{${writable}} writable for tests.")
+		comments.commentBlock("Writable", "Making the folders ${basePath}/{${writable}} writable for tests.")
 		makeWritableFolders.each {
-			isWritableInDocker(it, basePath, container, permissions)
+			isWritable.isWritableInDocker(it, basePath, container, permissions)
 		}
 		dir(basePath) {
 			File cThreeErrorLog = new File("${basePath}/c3_error.log")
-			commentBlock("Remote Coverage Error Log", "Making sure the ${cThreeErrorLog.toString()} exists and is writable.")
+			comments.commentBlock("Remote Coverage Error Log", "Making sure the ${cThreeErrorLog.toString()} exists and is writable.")
 			if (!cThreeErrorLog.exists()) {
 				sh "echo \"#c3_error.log\" > c3_error.log"
 			}
@@ -79,13 +79,13 @@ def runCodeceptionTests(String basePath, String container, String permissions, S
 	try {
 		dir(basePath) {
 			String flags
-			if (isMainRepoAndMaster(upstreamUser)) {
+			if (gitlab.isMainRepoAndMaster(upstreamUser)) {
 				flags = "--fail-fast --coverage --coverage-html --coverage-xml"
-				commentBlock("Codeception tests", "Running Codeception tests as \"${upstreamUser}\" with flags \"${flags}\"")
+				comments.commentBlock("Codeception tests", "Running Codeception tests as \"${upstreamUser}\" with flags \"${flags}\"")
 				sh "docker-compose exec -T ${container} bash -c \"vendor/bin/codecept run ${flags}\""
 			} else {
 				flags = "--fail-fast --coverage --coverage-html --coverage-xml --steps"
-				commentBlock("Codeception tests", "Running Codeception tests with flags \"${flags}\"")
+				comments.commentBlock("Codeception tests", "Running Codeception tests with flags \"${flags}\"")
 				sh "docker-compose exec -T ${container} bash -c \"vendor/bin/codecept run ${flags}\""
 			}
 		}
