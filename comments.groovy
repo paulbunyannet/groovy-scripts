d7eb7021a6cc89010e33abc139a20de31c3eeea9
Nate Nolting <me@natenolting.com>
2018-04-03 22:18:08 -0500

    Trying a different split method

--- comments.groovy
+++ comments.groovy
@@ -3,7 +3,7 @@ def commentBlock(String title="", String body="" ) {
 	Integer lineLength = 75
 	String linePrefix = "    | "
 	//parsedBody = sh(script:"echo ${StringEscapeUtils.escapeJava(body)} | sed -e \"s/.\\{${lineLength}\\}/&\\n${linePrefix}/g\"", returnStdout: true)
-	String[] lines = body.split("(?<=\\G.{${lineLength})")
+	String[] lines = body.split("(?<=\\G.{${lineLength}})")
 	String lineJoin = "\n${linePrefix}"
 	echo "    /*\n" +
 			"    |${'-'.multiply(lineLength)}\n" +
@@ -12,5 +12,5 @@ def commentBlock(String title="", String body="" ) {
 			"    |\n" +
 			"    | ${lines.join(lineJoin)}\n" +
 			"    */"
-} 
+}
 return this
