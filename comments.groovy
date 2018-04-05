9d453fddb9d90a12f47e642295beef1e46c3a37e
Nate Nolting <me@natenolting.com>
2018-04-05 09:56:20 -0500

    removed extra space at start and end of comment block so it lands inline with the rest of the console log.

--- comments.groovy
+++ comments.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/groovy
 def commentBlock(String title="", String body="" ) {
 	Integer lineLength = 75
-	String linePrefix = "    | "
+	String linePrefix = " | "
 	//parsedBody = sh(script:"echo ${StringEscapeUtils.escapeJava(body)} | sed -e \"s/.\\{${lineLength}\\}/&\\n${linePrefix}/g\"", returnStdout: true)
 	String[] lines = body.split("(?<=\\G.{${lineLength}})")
 	String lineJoin = "\n${linePrefix}"
-	echo "    /*\n" +
-			"    |${'-'.multiply(lineLength)}\n" +
-			"    | ${title}\n" +
-			"    |${'-'.multiply(lineLength)}\n" +
-			"    |\n" +
-			"    | ${lines.join(lineJoin)}\n" +
-			"    */"
+	echo "/*\n" +
+			" |${'-'.multiply(lineLength)}\n" +
+			" | ${title}\n" +
+			" |${'-'.multiply(lineLength)}\n" +
+			" |\n" +
+			" | ${lines.join(lineJoin)}\n" +
+			"*/"
 }
 return this
