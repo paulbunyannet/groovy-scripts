3b527691198bafc715147881aa9e8f6ea3de0ef9
Nate Nolting <me@natenolting.com>
2018-04-20 10:40:10 -0500

    Updated generateMD5 hash function

--- hash.groovy
+++ hash.groovy
@@ -1,10 +1,17 @@
 def version = '1.0'
 
-import java.security.MessageDigest
-
-//https://gist.github.com/ikarius/299062#gistcomment-1463377
-def generateMD5(String s){
-    MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
+/**
+ * MD5 a file contents
+ * https://gist.github.com/mcquinne/2655782
+ * @param file
+ * @return
+ */
+def generateMD5( File file ) {
+    def digest = java.security.MessageDigest.getInstance("MD5")
+    file.eachByte( 4096 ) { buffer, length ->
+        digest.update( buffer, 0, length )
+    }
+    new BigInteger(1, digest.digest()).toString(16).padLeft(32, '0')
 }
 
-return this;
+return this
